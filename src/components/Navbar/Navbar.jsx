7c37e162daff9bf7bace6f1b0fae11ce74bbcfff
lqasemzadeh <lqasemzadeh@gmail.com>
2024-06-15 20:00:48 +0330

    Simplify Hero component and refactor navigation links
    The content of the Hero component is significantly simplified, removing multiple icon and text elements. The navigation links in the Navbar component have been refactored, replacing NavLink with Link and adding padding and styles directly in the component rather than in the CSS file. Changes to styling also moved from the CSS file to being applied directly in the components.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import { FiAlignJustify, FiX } from "react-icons/fi";
 import Logo from "../../assets/images/SITT.png"
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     const [nav, setNav] = useState(false)
@@ -13,16 +13,17 @@ const Navbar = () => {
                 <div className="flex items-center">
                     <img src={Logo} className="h-8 w-auto"/>
                     <ul className="hidden md:flex">
-                        <li><NavLink to="/">خانه</NavLink></li>
-                        <li><NavLink to="/projects">پروژه‌ها</NavLink></li>
-                        <li><NavLink to="/services">خدمات</NavLink></li>
-                        <li><NavLink to="/news">اخبار</NavLink></li>
-                        <li><NavLink to="/about">درباره ما</NavLink></li>
-                        <li><NavLink to="/contact">تماس با ما</NavLink></li>
+                        <li className="p-4"><Link to="/">خانه</Link></li>
+                        <li className="p-4"><Link to="/projects">پروژه‌ها</Link></li>
+                        <li className="p-4"><Link to="/services">خدمات</Link></li>
+                        <li className="p-4"><Link to="/news">اخبار</Link></li>
+                        <li className="p-4"><Link to="/about">درباره ما</Link></li>
+                        <li className="p-4"><Link to="/contact">تماس با ما</Link></li>
                     </ul>
                 </div>
                 <div className="hidden md:flex pr-4">
-                    <button className="px-5 py-1">پنل‌ها</button>
+                    <button className="px-5 py-1 text-white border-2 bg-[#ff9500] border-[#352d22]
+        hover:bg-transparent hover:text-[#352d22] rounded-md">پنل‌ها</button>
                 </div>
                 <div className="md:hidden"
                      onClick={handleClick}
@@ -31,26 +32,27 @@ const Navbar = () => {
                 </div>
             </div>
             <ul className={!nav ? "hidden" : "absolute bg-zinc-200 w-full px-8"}>
-                <li className="border-b-2 border-zinc-300 w-full text-center">
-                    <NavLink to="/">خانه</NavLink>
+                <li className="p-4 border-b-2 border-zinc-300 w-full text-center">
+                    <Link to="/">خانه</Link>
                 </li>
-                <li className="border-b-2 border-zinc-300 w-full text-center">
-                    <NavLink to="/projects">پروژه‌ها</NavLink>
+                <li className="p-4 border-b-2 border-zinc-300 w-full text-center">
+                    <Link to="/projects">پروژه‌ها</Link>
                 </li>
-                <li className="border-b-2 border-zinc-300 w-full text-center">
-                    <NavLink to="/services">خدمات</NavLink>
+                <li className="p-4 border-b-2 border-zinc-300 w-full text-center">
+                    <Link to="/services">خدمات</Link>
                 </li>
-                <li className="border-b-2 border-zinc-300 w-full text-center">
-                    <NavLink to="/news">اخبار</NavLink>
+                <li className="p-4 border-b-2 border-zinc-300 w-full text-center">
+                    <Link to="/news">اخبار</Link>
                 </li>
-                <li className="border-b-2 border-zinc-300 w-full text-center">
-                    <NavLink to="/about">درباره ما</NavLink>
+                <li className="p-4 border-b-2 border-zinc-300 w-full text-center">
+                    <Link to="/about">درباره ما</Link>
                 </li>
-                <li className="border-b-2 border-zinc-300 w-full text-center">
-                    <NavLink to="/contact">تماس با ما</NavLink>
+                <li className="p-4 border-b-2 border-zinc-300 w-full text-center">
+                    <Link to="/contact">تماس با ما</Link>
                 </li>
                 <div className="flex flex-col my-4">
-                    <button className="px-5 py-1">پنل‌ها</button>
+                    <button className="px-5 py-1 text-white border-2 bg-[#ff9500] border-[#352d22]
+        hover:bg-transparent hover:text-[#352d22] rounded-md">پنل‌ها</button>
                 </div>
             </ul>
         </div>
