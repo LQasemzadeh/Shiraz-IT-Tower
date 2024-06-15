c4beca301d9378175a0dcbf9faf7af91d1ec93a3
lqasemzadeh <lqasemzadeh@gmail.com>
2024-06-15 14:33:25 +0330

    Remove DarkMode and Hero components, update others
    Deleted DarkMode.jsx and Hero.jsx components from the 'src/components' directory. Changes were also made to several other files, including the Navbar.jsx file where the DarkMode and Hero components were formerly imported. Additionally, tailwind configuration and index styling was simplified, and a small section regarding panels were added to the Navbar component.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { FiAlignJustify, FiX } from "react-icons/fi";
 import Logo from "../../assets/images/SITT.png"
 
 
 const Navbar = () => {
+    const [nav, setNav] = useState(false)
+    const handleClick = () => setNav(!nav)
+
+
     return (
-        <div className="w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg">
+        <div className="w-screen h-[70px] z-10 bg-zinc-200 fixed drop-shadow-lg">
             <div className="px-14 flex justify-between items-center w-full h-full">
                 <div className="flex items-center">
                     <img src={Logo} className="h-8 w-auto"/>
@@ -19,12 +23,28 @@ const Navbar = () => {
                     </ul>
                 </div>
                 <div className="hidden md:flex pr-4">
-                    <button>پنل‌ها</button>
+                    <button className="px-5 py-1">پنل‌ها</button>
                 </div>
+                <div className="md:hidden"
+                onClick={handleClick}
+                >
+                    {!nav ? <FiAlignJustify className="w-10"/> : <FiX className="w-10"/>}
 
+                </div>
             </div>
-            <FiAlignJustify className="w-10"/>
-            <FiX className="w-10"/>
+
+            <ul className={!nav ? "hidden" : "absolute bg-zinc-200 w-full px-8"}>
+                <li className="border-b-2 border-zinc-300 w-full text-center">خانه</li>
+                <li className="border-b-2 border-zinc-300 w-full text-center">پروژه‌ها</li>
+                <li className="border-b-2 border-zinc-300 w-full text-center">خدمات</li>
+                <li className="border-b-2 border-zinc-300 w-full text-center">اخبار</li>
+                <li className="border-b-2 border-zinc-300 w-full text-center">درباره ما</li>
+                <li className="border-b-2 border-zinc-300 w-full text-center">تماس با ما</li>
+                <div className="flex flex-col my-4">
+                    <button className="px-5 py-1">پنل‌ها</button>
+                </div>
+            </ul>
+
         </div>
     );
 };
