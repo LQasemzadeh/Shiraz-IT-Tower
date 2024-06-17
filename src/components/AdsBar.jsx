c56f4c35154bd6453adb7c71250862643905b5c0
lqasemzadeh <lqasemzadeh@gmail.com>
2024-06-17 18:16:46 +0330

    Add AdsBar component and refactor Hero component
    In this update, the AdsBar component was introduced into the home page to handle advertisements. Some changes were also made to the Hero component's file structure and styling. Additional dependencies, including 'react-slick' and 'slick-carousel', were added to support these changes.

---
+++ src/components/AdsBar.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "../App.css";
+import mega from "../../src/assets/images/saham/mega.png";
+import logo from "../../src/assets/images/saham/Logo-Compress.png";
+import shiraz from "../../src/assets/images/saham/Shiraz_Especial_Economic_Zone.png";
+import layer from "../../src/assets/images/saham/Layer-82.png";
+
+
+const AdsBar = () => {
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 2000,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        cssEase: "linear"
+    };
+
+    return (
+        <div className="bg-white">
+            <div className="mx-auto max-w-2xl px-4 py-10 sm:px-6 sm:py-12 lg:max-w-7xl lg:px-8">
+                <div className="mainContainer">
+                    <Slider {...settings}>
+                        <div className="container">
+                            <img src={mega} alt="Falat"/>
+                        </div>
+                        <div className="container">
+                            <img src={logo} alt="Jam"/>
+                        </div>
+                        <div className="container">
+                            <img src={shiraz} alt="Marun"/>
+                        </div>
+                        <div className="container">
+                            <img src={layer} alt="Pasargad"/>
+                        </div>
+
+                    </Slider>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AdsBar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import Hero from "../components/Hero/Hero.jsx";
+import Hero from "../components/Hero.jsx";
+import AdsBar from "../components/AdsBar.jsx";
 
 const Home = () => {
 
     return (
         <div>
             <Hero />
+            <AdsBar />
         </div>
     );
 };
