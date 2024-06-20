9a3de6a05941057fd79381eb5304373928407ce5
lqasemzadeh <lqasemzadeh@gmail.com>
2024-06-20 17:49:30 +0330

    Update styling in Price component
    The commit introduces new class names and some changes in elements' structure in Price.jsx to improve the appearance and readability. The HTML structure of the Price panel has been adjusted with enhanced stylistic details allowing better user interface experience.

--- src/components/Price.jsx
+++ src/components/Price.jsx
@@ -3,12 +3,12 @@ import { FcCheckmark } from "react-icons/fc";
 
 const Price = () => {
     return (
-        <div>
-            <div>Absolute</div>
+        <div className="w-full text-white my-24">
+            <div className="w-full h-[800px] bg-slate-800 absolute mix-blend-overlay"></div>
 
-            <div>
-                <div>
-                    <h2>انتخاب پنل پیامکی مناسب</h2>
+            <div className="max-w-[1240px] mx-auto py-12">
+                <div className="text-center py-8 text-slate-300">
+                    <h2 className="text-3xl font-bold text-white">انتخاب پنل پیامکی مناسب</h2>
                     <p>سرويس‌های پیام‌دهی تحت وب با واسط کاربری ساده و کارآمد برای سازمان‌ها و شرکت‌ها</p>
                 </div>
 
@@ -20,12 +20,12 @@ const Price = () => {
                         </div>
                         <p>ویژه کسب و کارهای در حال رشد</p>
                         <div>
-                            <p><FcCheckmark/>خط 12 رقمی</p>
-                            <p><FcCheckmark/>ارسال پیشرفته</p>
-                            <p><FcCheckmark/>پیامک انبوه</p>
-                            <p><FcCheckmark/>خرید خط جدید</p>
-                            <p><FcCheckmark/>پیامکی بین 70 تا 85 تومان (به میزان خرید بستگی دارد)</p>
-                            <p><FcCheckmark/>500 پیامک</p>
+                            <p><FcCheckmark className="mr-5"/>خط 12 رقمی</p>
+                            <p><FcCheckmark className="mr-5"/>ارسال پیشرفته</p>
+                            <p><FcCheckmark className="mr-5"/>پیامک انبوه</p>
+                            <p><FcCheckmark className="mr-5"/>خرید خط جدید</p>
+                            <p><FcCheckmark className="mr-5"/>پیامکی بین 70 تا 85 تومان (به میزان خرید بستگی دارد)</p>
+                            <p><FcCheckmark className="mr-5"/>500 پیامک</p>
                             <button>شروع کنید</button>
                         </div>
                     </div>
