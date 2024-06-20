fdda940f3ede7fae628299209c9eb30bcf4d5a18
lqasemzadeh <lqasemzadeh@gmail.com>
2024-06-20 18:47:20 +0330

    Update styling and layout of Price component
    This update improves the readability of the Price component by adding styling attributes to the pricing information and layout attributes to improve the symmetry of multiple pricing cards. It also includes hover effects and button layouts to enhance user experience.

--- src/components/Price.jsx
+++ src/components/Price.jsx
@@ -12,21 +12,67 @@ const Price = () => {
                     <p>سرويس‌های پیام‌دهی تحت وب با واسط کاربری ساده و کارآمد برای سازمان‌ها و شرکت‌ها</p>
                 </div>
 
-                <div>
-                    <div>
-                        <span>پیشنهاد</span>
+                <div className="grid grid-cols-3">
+                    <div className="bg-white text-slate-900 m-4 p-8 rounded-xl shadow-2xl relative">
+                        <span className="px-3 py-1 bg-indigo-200 text-indigo-900 rounded-xl text-sm">پیشنهاد</span>
                         <div>
-                            <p>۳۵۹ هزارتومان<span>/ماهانه</span></p>
+                            <p className="text-xl font-bold py-4 flex">۳۵۹ هزارتومان<span
+                                className="text-lg text-slate-500 flex flex-col justify-center">/ماهانه</span></p>
                         </div>
-                        <p>ویژه کسب و کارهای در حال رشد</p>
+                        <p className="text-lg py-4 text-slate-500">ویژه کسب و کارهای در حال رشد</p>
                         <div>
-                            <p><FcCheckmark className="mr-5"/>خط 12 رقمی</p>
-                            <p><FcCheckmark className="mr-5"/>ارسال پیشرفته</p>
-                            <p><FcCheckmark className="mr-5"/>پیامک انبوه</p>
-                            <p><FcCheckmark className="mr-5"/>خرید خط جدید</p>
-                            <p><FcCheckmark className="mr-5"/>پیامکی بین 70 تا 85 تومان (به میزان خرید بستگی دارد)</p>
-                            <p><FcCheckmark className="mr-5"/>500 پیامک</p>
-                            <button>شروع کنید</button>
+                            <p className="flex py-3"><FcCheckmark className="mr-5"/>خط 12 رقمی</p>
+                            <p className="flex py-3"><FcCheckmark className="mr-5"/>ارسال پیشرفته</p>
+                            <p className="flex py-3"><FcCheckmark className="mr-5"/>پیامک انبوه</p>
+                            <p className="flex py-3"><FcCheckmark className="mr-5"/>خرید خط جدید</p>
+                            <p className="flex py-3"><FcCheckmark className="mr-5"/>پیامکی بین 70 تا 85 تومان (به میزان
+                                خرید بستگی دارد)</p>
+                            <p className="flex py-3"><FcCheckmark className="mr-5"/>500 پیامک</p>
+                            <button className="w-full px-5 py-1 text-white border-2 bg-[#ff9500] border-[#352d22]
+        hover:bg-transparent hover:text-[#352d22] rounded-full">شروع کنید
+                            </button>
+                        </div>
+                    </div>
+
+                    <div className="bg-white text-slate-900 m-4 p-8 rounded-xl shadow-2xl relative">
+                        <span className="px-3 py-1 bg-indigo-200 text-indigo-900 rounded-xl text-sm">پیشنهاد</span>
+                        <div>
+                            <p className="text-xl font-bold py-4 flex">۳۵۹ هزارتومان<span
+                                className="text-lg text-slate-500 flex flex-col justify-center">/ماهانه</span></p>
+                        </div>
+                        <p className="text-lg py-4 text-slate-500">ویژه کسب و کارهای در حال رشد</p>
+                        <div>
+                            <p className="flex py-3"><FcCheckmark className="mr-5"/>خط 12 رقمی</p>
+                            <p className="flex py-3"><FcCheckmark className="mr-5"/>ارسال پیشرفته</p>
+                            <p className="flex py-3"><FcCheckmark className="mr-5"/>پیامک انبوه</p>
+                            <p className="flex py-3"><FcCheckmark className="mr-5"/>خرید خط جدید</p>
+                            <p className="flex py-3"><FcCheckmark className="mr-5"/>پیامکی بین 70 تا 85 تومان (به میزان
+                                خرید بستگی دارد)</p>
+                            <p className="flex py-3"><FcCheckmark className="mr-5"/>500 پیامک</p>
+                            <button className="w-full px-5 py-1 text-white border-2 bg-[#ff9500] border-[#352d22]
+        hover:bg-transparent hover:text-[#352d22] rounded-full">شروع کنید
+                            </button>
+                        </div>
+                    </div>
+
+                    <div className="bg-white text-slate-900 m-4 p-8 rounded-xl shadow-2xl relative">
+                        <span className="px-3 py-1 bg-indigo-200 text-indigo-900 rounded-xl text-sm">پیشنهاد</span>
+                        <div>
+                            <p className="text-xl font-bold py-4 flex">۳۵۹ هزارتومان<span
+                                className="text-lg text-slate-500 flex flex-col justify-center">/ماهانه</span></p>
+                        </div>
+                        <p className="text-lg py-4 text-slate-500">ویژه کسب و کارهای در حال رشد</p>
+                        <div>
+                            <p className="flex py-3"><FcCheckmark className="mr-5"/>خط 12 رقمی</p>
+                            <p className="flex py-3"><FcCheckmark className="mr-5"/>ارسال پیشرفته</p>
+                            <p className="flex py-3"><FcCheckmark className="mr-5"/>پیامک انبوه</p>
+                            <p className="flex py-3"><FcCheckmark className="mr-5"/>خرید خط جدید</p>
+                            <p className="flex py-3"><FcCheckmark className="mr-5"/>پیامکی بین 70 تا 85 تومان (به میزان
+                                خرید بستگی دارد)</p>
+                            <p className="flex py-3"><FcCheckmark className="mr-5"/>500 پیامک</p>
+                            <button className="w-full px-5 py-1 text-white border-2 bg-[#ff9500] border-[#352d22]
+        hover:bg-transparent hover:text-[#352d22] rounded-full">شروع کنید
+                            </button>
                         </div>
                     </div>
                 </div>
