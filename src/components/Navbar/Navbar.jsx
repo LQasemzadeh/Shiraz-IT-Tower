65dfb8729a24252d33d1d23346892c8e58e8a9be
lqasemzadeh <lqasemzadeh@gmail.com>
2024-06-24 16:14:15 +0330

    drop down menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,6 +6,7 @@ import "../../components/Navbar/Navbar.css";
 
 
 const Navbar = () => {
+    const Menus = ['پنل پیامکی', 'پنل مدیریت ساختمان', 'پنل پرداخت یاری']
     const [nav, setNav] = useState(false)
     const [navbar, setNavbar] = useState(false);
     const handleClick = () => setNav(!nav)
@@ -25,17 +26,29 @@ const Navbar = () => {
                 <div className="flex items-center">
                     <img src={Logo} className="h-8 w-auto"/>
                     <ul className="hidden md:flex">
-                        <li className="p-4"><Link to="/">خانه</Link></li>
-                        <li className="p-4"><Link to="/projects">پروژه‌ها</Link></li>
-                        <li className="p-4"><Link to="/services">خدمات</Link></li>
-                        <li className="p-4"><Link to="/news">اخبار</Link></li>
-                        <li className="p-4"><Link to="/about">درباره ما</Link></li>
-                        <li className="p-4"><Link to="/contact">تماس با ما</Link></li>
+                        <li className="p-4 hover:bg-gray-300"><Link to="/">خانه</Link></li>
+                        <li className="p-4 hover:bg-gray-300"><Link to="/projects">پروژه‌ها</Link></li>
+                        <li className="p-4 hover:bg-gray-300"><Link to="/services">خدمات</Link></li>
+                        <li className="p-4 hover:bg-gray-300"><Link to="/news">اخبار</Link></li>
+                        <li className="p-4 hover:bg-gray-300"><Link to="/about">درباره ما</Link></li>
+                        <li className="p-4 hover:bg-gray-300"><Link to="/contact">تماس با ما</Link></li>
                     </ul>
                 </div>
                 <div className="hidden md:flex pr-4">
                     <button className="px-5 py-1 text-white border-2 bg-[#ff9500] border-[#352d22]
         hover:bg-transparent hover:text-[#352d22] rounded-full">پنل‌ها</button>
+                    <div className="absolute left-8 z-10 mt-10 w-48 origin-top-left rounded-md bg-white py-1
+                    shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none
+                    data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0
+                    data-[enter]:duration-200 data-[leave]:duration-75 data-[enter]:ease-out
+                    data-[leave]:ease-in">
+                        <ul className="">
+                            {Menus.map((menu) => (
+                                <li className="p-2 cursor-pointer rounded hover:bg-orange-100" key={menu}>{menu}</li>
+                            ))}
+
+                        </ul>
+                    </div>
                 </div>
                 <div className="md:hidden"
                      onClick={handleClick}
